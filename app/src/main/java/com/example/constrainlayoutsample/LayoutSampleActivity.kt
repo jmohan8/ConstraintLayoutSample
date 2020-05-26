040ac2ff6588b4b9b62d78b2b4e090a8c408d6b7
jmohan8 <jmscit11@gmail.com>
2020-05-26 05:54:18 +0530

    Constrain Layout Sample

---
+++ app/src/main/java/com/example/constrainlayoutsample/LayoutSampleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.constrainlayoutsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LayoutSampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_sample)
+    }
+}
